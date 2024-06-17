2fc7d0539faa378d6e5bbb7f23d2bbb49dd1675b
Darshan Choudhary <97291483+DarshanchGIT@users.noreply.github.com>
2024-06-17 20:04:30 +0530

    Added Vercel Analytics and upgraded React to 18.2.0

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+//to checking web vitals 
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
